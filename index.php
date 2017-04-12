d00b375ddebadaa40b7823997adb2413d170594a
wonexo <victorwonex@gmail.com>
2017-04-12 11:39:01 +0100

    coding..

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-    <body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>SwapIt</title>
+        <link rel="stylesheet" href="./vendor/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./vendor/font-awesome/css/font-awesome.css">
         
+        <link rel="stylesheet" href="./vendor/css/style.css">
+    </head>
+    <body>
+
+    scrpt 
     </body>
 </html>
